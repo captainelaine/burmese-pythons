b06d431dc62724fc1e500af1547498015b29e008
Captainelaine <zxf76@miami.edu>
2016-12-15 23:01:15 -0500

    fix the inside of python

--- js/pythoninside.js
+++ js/pythoninside.js
@@ -2,15 +2,24 @@ var topskinlayer=document.getElementsByClassName("fenjie");
 var writing=document.getElementById("fenjie3");
 
 
-topskinlayer[1].addEventListener("click",function(e){
-  if(e.target.style.left="30.8%"){
+topskinlayer[3].addEventListener("click",function(e){
+  if(e.target.style.left="44%"){
     topskinlayer[1].style.left = "10%";
+    topskinlayer[3].style.display = "none";
     document.getElementById("fenjie3").style.display="block";
   }
 
-  // } if(e.target.style.left = "10%") {
+  // if(e.target.style.style = "10%") {
   //   topskinlayer[1].style.left="30.8";
   //   // document.getElementById("fenjie3").style.display="none";
   // }
 
+})
+
+topskinlayer[1].addEventListener("click",function(e){
+  if(e.target.style.left="54%") {
+    topskinlayer[1].style.left="30.8%";
+    topskinlayer[3].style.display = "block";
+    document.getElementById("fenjie3").style.display="none";
+  }
 })
